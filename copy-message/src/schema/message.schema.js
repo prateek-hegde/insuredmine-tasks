83f8f67fdab835e1067f782ef5700ec0ea07e344
Prateek Hegde <prateek@Prateeks-MacBook-Pro.local>
2022-05-30 01:23:35 +0530

    postman collection added

--- copy-message/src/schema/message.schema.js
+++ copy-message/src/schema/message.schema.js
@@ -2,9 +2,7 @@ const joi = require('joi')
 
 const messageSchema = joi.object({
     message: joi.string().required().max(255),
-    // .messages(joiStringErrorMessage('title', true, 50)),
     timeStamp: joi.string().required(),
-    // .messages(joiStringErrorMessage('content', true, 255)),
 })
 
 module.exports = { messageSchema }
